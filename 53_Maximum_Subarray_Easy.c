71d8ddebbaa9b6b5b68ab801b3817f78df7eab9d
jenslee <913470452@qq.com>
2017-04-23 10:00:09 +0800

    Update 53_Maximum_Subarray_Easy.c

--- 53_Maximum_Subarray_Easy.c
+++ 53_Maximum_Subarray_Easy.c
@@ -6,6 +6,8 @@ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
 本题使用Kadane's Algorithm
 算法核心，一个连续数组的最大和，只能是这个数字nums[i]本身或者这个数字加上前面数组数值nums[i]+general_max累加的最大值，从而获得最大的累计和
+
+具体算法描述：https://www.linkedin.com/pulse/kadanes-algorithm-mustafa-bedir-tapkan
 */
 int maxSubArray(int* nums, int numsSize) {
     int i;
