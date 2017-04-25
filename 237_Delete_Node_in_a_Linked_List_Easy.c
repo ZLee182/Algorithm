dffc12b6e753f57f507d638210425f12445df121
jenslee <913470452@qq.com>
2017-04-25 22:37:16 +0800

    删除单链表节点
    删除单链表节点

---
+++ 237_Delete_Node_in_a_Linked_List_Easy.c
@@ -0,0 +1,32 @@
+/*
+给定一个单链表，参数是要删除的节点
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function. 
+*/
+//解法一  40%  3ms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+
+//解法二   4%  6ms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    *node = *node->next;
+}
