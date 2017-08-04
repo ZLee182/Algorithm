ed29bf5b7cb65a81bd22ab447ff5a559acf6c379
jenslee <913470452@qq.com>
2017-08-04 15:35:24 +0800

    数据库：交换男女性别

---
+++ 627. Swap Salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+#Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update query and no intermediate temp table.
+UPDATE salary
+    SET sex = (
+        CASE WHEN sex = 'm'
+            THEN 'f'
+            ELSE 'm'
+            END
+    )
