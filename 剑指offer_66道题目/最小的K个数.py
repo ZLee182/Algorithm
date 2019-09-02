57ce692cbee6f6690e99df22ac121fe72c86ed8d
jenslee <913470452@qq.com>
2019-09-02 21:51:59 +0800

    Update 最小的K个数.py

--- 剑指offer_66道题目/最小的K个数.py
+++ 剑指offer_66道题目/最小的K个数.py
@@ -1,3 +1,6 @@
+
+#输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+
 # -*- coding:utf-8 -*-
 class Solution:
     def GetLeastNumbers_Solution(self, tinput, k):
@@ -5,3 +8,7 @@ class Solution:
         if k > len(tinput):
             return []
         return sorted(tinput)[0:k]
+    
+'''
+思路：首先检查剩余的字符串是否足够长度，然后排序，筛选出符合需求的数组
+'''
